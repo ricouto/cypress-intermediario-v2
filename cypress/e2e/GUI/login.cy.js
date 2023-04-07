6d7940904dcb1837a35f69187217aeceb50de15a
ricouto <ricardocaputo@gmail.com>
2023-04-07 09:02:43 -0300

    test case logout

--- cypress/e2e/GUI/login.cy.js
+++ cypress/e2e/GUI/login.cy.js
@@ -1,10 +1,23 @@
 describe('Suite GitLab', () => {
+
+  beforeEach(() => {
+    cy.login()
+  })
+
   it('#1 - cenario de login com sucesso', () => {
-    cy.visit('/users/sign_in')
-    cy.get('[data-qa-selector="login_field"]').type(Cypress.env('user_name'))
-    cy.get('[data-qa-selector="password_field"]').type(Cypress.env('user_password'), {log: false})
-    cy.get('[data-qa-selector="sign_in_button"]').click()
+    cy.get('[data-qa-selector="welcome_title_content"]')
+    .should('be.visible')
+    .and('contain', 'Welcome to GitLab')
 
     cy.get('[data-qa-selector="user_menu"]').should('be.visible')
+  })
+
+  it('#2 - cenario de logout com sucesso', () => {
+    cy.get('[data-qa-selector="user_menu"]').click()
+    cy.get('[data-qa-selector="sign_out_link"]').click()
+
+    cy.get('[data-qa-selector="sign_in_button"]').should('be.visible')
+
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
   })
 })
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (
+    user = Cypress.env('user_name'),
+    password = Cypress.env('user_password'),
+) => {
+    const login = () => {
+        cy.visit('/users/sign_in')
+        cy.get('[data-qa-selector="login_field"]').type(user)
+        cy.get('[data-qa-selector="password_field"]').type(password, { log: false })
+        cy.get('[data-qa-selector="sign_in_button"]').click()
+    }
+    login()
+})
